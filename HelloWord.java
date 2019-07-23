5de7c24c3c01b94ee9d230cd1a3f2fac19aab578
duybvk <duy20115499@gmail.com>
2019-07-23 09:55:19 +0700

    update file

--- HelloWord.java
+++ HelloWord.java
@@ -1,6 +1,6 @@
 
 public class HelloWord {
 	public static void main(String[] args) {
-		System.out.println("Jenkins integration Git !");
+		System.out.println("Jenkins integration Git ! !!!");
 	}
 }
